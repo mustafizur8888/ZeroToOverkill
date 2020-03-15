de3fee20c2c21e3a22f7043b404624664b7d0ef5
Mustafizur Rahman <mustafizur35@outlook.com>
2020-03-15 11:55:25 +0600

    added NLog

--- ZeroToOverkill/Program.cs
+++ ZeroToOverkill/Program.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using NLog.Web;
+using LogLevel = NLog.LogLevel;
 
 namespace ZeroToOverkill
 {
@@ -14,11 +19,28 @@ namespace ZeroToOverkill
     {
         public static void Main(string[] args)
         {
+            ConfigureNLog();
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        private static void ConfigureNLog()
+        {
+            var config = new LoggingConfiguration();
+            var consoleTarget = new ColoredConsoleTarget("coloredConsole")
+            {
+                Layout = @"${data:format=HH\:mm\:ss} ${level} ${message} ${exception}"
+            };
+            config.AddTarget(consoleTarget);
+            config.AddRule(LogLevel.Info,LogLevel.Fatal, consoleTarget);
+            LogManager.Configuration = config;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                })
+                .UseNLog()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {

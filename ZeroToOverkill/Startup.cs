f26885f7b00f7dad60350431a9d3e5003226a0f1
mustafizur8888 <mustafizur8888@gmail.com>
2020-03-17 13:08:31 +0600

    app run for no middleware found

--- ZeroToOverkill/Startup.cs
+++ ZeroToOverkill/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -54,6 +55,10 @@ namespace ZeroToOverkill
             app.UseMiddleware<RequestTimingAdHocMiddleware>();
             app.UseMiddleware<RequestTimingFactoryMiddleware>();
             app.UseMvc();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("No middleware could handle the request");
+            });
         }
     }
 }
